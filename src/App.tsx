5892c273c86ff6e0e7fd9ef43d66583ac549178c
Ross Kuyper <ross@rosskuyper.com>
2020-07-27 13:07:38 +0200

    Raw foundations of login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,113 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Avatar from '@material-ui/core/Avatar'
+import Box from '@material-ui/core/Box'
+import Button from '@material-ui/core/Button/Button'
+import Container from '@material-ui/core/Container'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Link from '@material-ui/core/Link'
+import {makeStyles} from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
+import React from 'react'
+import {gql, useQuery} from '@apollo/client'
+import queryString from 'query-string'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(10),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(0, 0, 4),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  submit: {
+    margin: theme.spacing(0, 0, 0),
+  },
+  copyright: {
+    position: 'fixed',
+    padding: theme.spacing(0, 0, 3),
+    bottom: 0,
+    left: 0,
+    width: '100%',
+  },
+}))
+
+const GET_SIGN_IN_OPTIONS = gql`
+  query {
+    getSignInOptions {
+      clientId
+      authorizeUri
+      responseType
+      redirectUri
+      scope
+      identityProvider
+    }
+  }
+`
+
+type SignInOption = {
+  clientId: string
+  authorizeUri: string
+  responseType: string
+  redirectUri: string
+  scope: string
+  identityProvider: string
 }
 
-export default App;
+const constructAuthorizeUrl = (option: SignInOption): string => {
+  return queryString.stringifyUrl({
+    url: option.authorizeUri,
+    query: {
+      authorize_uri: option.authorizeUri,
+      client_id: option.clientId,
+      response_type: option.responseType,
+      redirect_uri: option.redirectUri,
+      scope: option.scope,
+      identity_provider: option.identityProvider,
+    },
+  })
+}
+
+export default function SignIn() {
+  const classes = useStyles()
+  const {data} = useQuery(GET_SIGN_IN_OPTIONS)
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+
+        {data &&
+          data.getSignInOptions.map((option: SignInOption) => {
+            return (
+              <Button
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+                href={constructAuthorizeUrl(option)}
+                key={option.identityProvider}
+              >
+                Sign in with Google
+              </Button>
+            )
+          })}
+      </div>
+
+      <Box className={classes.copyright}>
+        <Typography variant="body2" color="textSecondary" align="center">
+          {'© '}
+          <Link color="inherit" href="https://habitualizer.com">
+            LyfApp
+          </Link>{' '}
+          {new Date().getFullYear()}
+        </Typography>
+      </Box>
+    </Container>
+  )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import {ApolloProvider, ApolloClient, InMemoryCache} from '@apollo/client'
+import './index.css'
+
+const client = new ApolloClient({
+  uri: process.env.REACT_APP_API_URL,
+  cache: new InMemoryCache(),
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  document.getElementById('root'),
+)
