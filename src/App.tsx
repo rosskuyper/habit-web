0e81fff5f08564254a8d5449c0e8e45b3b98b3bf
Ross Kuyper <ross@rosskuyper.com>
2020-08-11 15:10:41 +0200

    Add github package to test ci build

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import Loading from './components/Loading/Loading'
 import {useAuth} from './hooks/useAuth'
 import PublicSection from './sections/PublicSection'
 import UserSection from './sections/UserSection'
+import {useFriendStatus} from '@rosskuyper/ts-package-boilerplate'
 
 const App = () => {
   const {user} = useAuth()
+  useFriendStatus()
 
   // If we're still loading
   if (user === undefined) {
